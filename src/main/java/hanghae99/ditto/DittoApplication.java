d4a37e9f61066525dfccfd0a18b61b1c173851a0
minsung <als0318tjd@gmail.com>
2024-01-23 17:46:57 +0900

    first commit

---
+++ src/main/java/hanghae99/ditto/DittoApplication.java
@@ -0,0 +1,13 @@
+package hanghae99.ditto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DittoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DittoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hanghae99/ditto/DittoApplicationTests.java
@@ -0,0 +1,13 @@
+package hanghae99.ditto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DittoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
