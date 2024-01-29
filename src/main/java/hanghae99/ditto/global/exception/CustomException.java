887f1b7909a440f9ab946b8916a52f31b9cf49f0
minsung <als0318tjd@gmail.com>
2024-01-29 22:23:59 +0900

    chore. 사소한 변경

--- src/main/java/hanghae99/ditto/global/exception/CustomException.java
+++ src/main/java/hanghae99/ditto/global/exception/CustomException.java
@@ -4,5 +4,5 @@ public class CustomException extends RuntimeException{
 
     private CustomException() {}
 
-    protected CustomException(String message) {super(message);}
+    public CustomException(String message) {super(message);}
 }
