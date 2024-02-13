bcf8d463df44e0964e92a80d2b37e8b8f2c78805
minsung <als0318tjd@gmail.com>
2024-02-13 20:15:18 +0900

    feat. 구간별 해당 날짜 종가, 거래량 데이터 제공

--- ditto-module-batch/src/main/java/org/example/repository/PricePerDayRepository.java
+++ ditto-module-batch/src/main/java/org/example/repository/PricePerDayRepository.java
@@ -3,5 +3,12 @@ package org.example.repository;
 import org.example.domain.PricePerDay;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PricePerDayRepository extends JpaRepository<PricePerDay, Long> {
+
+    List<PricePerDay> findTop25ByCompanyIdOrderByDateDesc(Long companyId);
+    List<PricePerDay> findTop300ByCompanyIdOrderByDateDesc(Long companyId);
+    List<PricePerDay> findTop900ByCompanyIdOrderByDateDesc(Long companyId);
+    List<PricePerDay> findTop1500ByCompanyIdOrderByDateDesc(Long companyId);
 }
