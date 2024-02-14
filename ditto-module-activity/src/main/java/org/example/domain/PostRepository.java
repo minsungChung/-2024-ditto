eefc4c9630e108776e7db0e8f5e18b9058aac8b4
minsung <als0318tjd@gmail.com>
2024-02-14 20:59:22 +0900

    feat. 제목, 작성자, 내용으로 게시글 검색

--- ditto-module-activity/src/main/java/org/example/domain/PostRepository.java
+++ ditto-module-activity/src/main/java/org/example/domain/PostRepository.java
@@ -8,4 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findAllByStockId(Long stockId, Pageable pageable);
+
+    Page<Post> findAllByTitleContains(String title, Pageable pageable);
+
+    Page<Post> findAllByContentContains(String content, Pageable pageable);
+
+    Page<Post> findAllByMemberName(String memberName, Pageable pageable);
 }
