cea533f66a7ebd17cb7e91a6ae26d3f3dbdf5d13
minsung <als0318tjd@gmail.com>
2024-02-26 23:46:42 +0900

    bug. 낙관적 락을 통한 댓글 좋아요 동시성 이슈 해결

--- ditto-module-activity/src/main/java/org/example/domain/CommentLike.java
+++ ditto-module-activity/src/main/java/org/example/domain/CommentLike.java
@@ -4,36 +4,30 @@ import jakarta.persistence.Column;
 import org.example.global.entity.BaseEntity;
 import org.example.global.entity.UsageStatus;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentLike extends BaseEntity {
-    @Column(name = "=member_id", nullable = false)
+    @Column(name = "member_id", nullable = false)
     private Long memberId;
 
-    @ManyToOne
-    @JoinColumn(name = "comment_id", nullable = false)
-    private Comment comment;
+    @Column(name = "comment_id", nullable = false)
+    private Long commentId;
 
     @Builder
-    public CommentLike(Long memberId, Comment comment){
+    public CommentLike(Long memberId, Long commentId){
         this.memberId = memberId;
-        this.comment = comment;
+        this.commentId = commentId;
         this.status = UsageStatus.ACTIVE;
-        comment.addLike();
     }
 
     public void deleteCommentLike(){
         this.status = UsageStatus.DELETED;
-        comment.subLike();
     }
 
     public void pushCommentLike(){
         this.status = UsageStatus.ACTIVE;
-        comment.addLike();
     }
 }
